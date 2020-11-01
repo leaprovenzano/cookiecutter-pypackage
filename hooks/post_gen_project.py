a83f39e1e2815901443cfa2cbd6f64b24db7f11d
lea <leaprovenzano@gmail.com>
2020-11-01 08:56:09 +0000

    optional rtd config

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -14,10 +14,12 @@ if __name__ == '__main__':
         remove_file('AUTHORS.rst')
         remove_file('docs/authors.rst')
 
-
     if 'no' in '{{ cookiecutter.command_line_interface|lower }}':
         cli_file = os.path.join('src', '{{ cookiecutter.project_slug }}', 'cli.py')
         remove_file(cli_file)
 
     if 'Not open source' == '{{ cookiecutter.open_source_license }}':
         remove_file('LICENSE')
+
+    if '{{ cookiecutter.rtd }}' != 'y':
+        remove_file('.readthedocs.yaml')
